6d1099cb38aaeedecb4bb2d18965344881bfec24
Lieven Peeters <lieven.peeters@faros.be>
2015-09-10 11:15:12 +0200

    chatbox: trying to change from socket.io to sockjs

--- chatbox_server/app.js
+++ chatbox_server/app.js
@@ -4,45 +4,84 @@
 
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io')(server);
+    cors = require('cors'),
+    http = require('http'),
+    sockjs = require('sockjs'),
+    chatbox = sockjs.createServer({sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js'}),
+    server = http.createServer();
+    //server = require('http').createServer(app),
+    //io = require('socket.io')(server);
 
-var connectedUsers = [],
-    storeConnectedUser = function(user){
-        connectedUsers.push(user);
-    },
-    storeDisconnectedUser = function(username){
-        connectedUsers.splice(getIndexConnectedUserByUsername(username), 1);
-    },
-    getIndexConnectedUserByUsername = function(username){
-        for(var i = 0; i < connectedUsers.length; i++){
-            if(connectedUsers[i].username === username){
-                return i;
-            }
-        }
-        return undefined;
-    };
+var clients = [];
 
-io.on('connection', function(client){
-    client.on('join', function(username){
-        client.username = username;
-        storeConnectedUser({username: username, id: client.id, messages: []});
-        client.broadcast.emit('addChatter', {username: username, id: client.id, messages: []});
-        client.emit('initialLoad', {users: connectedUsers, messages: []});
+app.use(cors({origin:'*'}));
+
+chatbox.installHandlers(server, {prefix:'/chatbox'});
+chatbox.on('connection', function(client){
+    console.log('client connected');
+
+    //adds this client to the clients list
+    clients[client.id] = client;
+
+    client.on('data', function(message) {
+        console.log(message);
+        broadcast(JSON.parse(message));
     });
-    client.on('disconnect', function(){
-        storeDisconnectedUser(client.username);
-        client.broadcast.emit('removeChatter', client.username);
+
+    // on connection close event
+    client.on('close', function() {
+        delete clients[client.id];
     });
-    client.on('sendMessage', function(message){
-        client.broadcast.emit('newMessage', {sender: client.username, message: message});
-        client.emit('newMessage', {sender: client.username, message: message});
-    });
-    client.on('sendPrivateMessage', function(data){
-        client.emit('newPrivateMessage', {sender: client.username, receiver: data.receiver.username, message: data.message});
-        client.to(data.receiver.id).emit('newPrivateMessage', {sender: client.username, receiver: data.receiver.username, message: data.message});
-    });
+
 });
+
+// Broadcast to all clients
+function broadcast(message){
+    // iterate through each client in clients object
+    for (var client in clients){
+        // send the message to that client
+        clients[client].write(JSON.stringify(message));
+    }
+}
+
+
+//var connectedUsers = [],
+//    storeConnectedUser = function(user){
+//        connectedUsers.push(user);
+//    },
+//    storeDisconnectedUser = function(username){
+//        connectedUsers.splice(getIndexConnectedUserByUsername(username), 1);
+//    },
+//    getIndexConnectedUserByUsername = function(username){
+//        for(var i = 0; i < connectedUsers.length; i++){
+//            if(connectedUsers[i].username === username){
+//                return i;
+//            }
+//        }
+//        return undefined;
+//    };
+
+
+//io.on('connection', function(client){
+//    client.on('join', function(username){
+//        client.username = username;
+//        storeConnectedUser({username: username, id: client.id, messages: []});
+//        client.broadcast.emit('addChatter', {username: username, id: client.id, messages: []});
+//        client.emit('initialLoad', {users: connectedUsers, messages: []});
+//    });
+//    client.on('disconnect', function(){
+//        storeDisconnectedUser(client.username);
+//        client.broadcast.emit('removeChatter', client.username);
+//    });
+//    client.on('sendMessage', function(message){
+//        client.broadcast.emit('newMessage', {sender: client.username, message: message});
+//        client.emit('newMessage', {sender: client.username, message: message});
+//    });
+//    client.on('sendPrivateMessage', function(data){
+//        client.emit('newPrivateMessage', {sender: client.username, receiver: data.receiver.username, message: data.message});
+//        client.to(data.receiver.id).emit('newPrivateMessage', {sender: client.username, receiver: data.receiver.username, message: data.message});
+//    });
+//});
 
 server.listen(9998, function(){
     console.log("Listening on port 9998");
