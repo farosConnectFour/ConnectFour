367d4700162b77fb032576fd002b36550946d574
juchtdi <dimitri.juchtmans@faros.be>
2015-09-11 13:57:09 +0200

    changed createScript for moar users

--- db/Create.sql
+++ db/Create.sql
@@ -9,9 +9,17 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (userId, name, password, points) values
-(1,"Dimitri", "pass",1100),
-(2,"Lieven", "pass",1200),
-(3,"Kwinten", "pass",850),
-(4,"Sara", "pass",975),
-(5,"Peter", "pass",925),
-(6,"Michael", "pass",950)
+(1,"Dimitri", "pass",1350),
+(2,"Lieven", "pass",1300),
+(3,"Kwinten", "pass",1250),
+(4,"Sara", "pass",1200),
+(5,"Peter", "pass",1150),
+(6,"Michael", "pass",1100),
+(7,"Christophe", "pass",1050),
+(8,"Wim", "pass",1000),
+(9,"Johan", "pass",950),
+(10,"Jessica", "pass",900),
+(11,"Jeroen", "pass",850),
+(12,"Sven", "pass",800),
+(13,"Joris", "pass",750),
+(14,"Tom", "pass",700)
