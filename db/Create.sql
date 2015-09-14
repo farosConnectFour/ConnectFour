087948f1fc6e3e3d38a8c0b7e208089e36d4137e
juchtdi <dimitri.juchtmans@faros.be>
2015-09-14 10:29:04 +0200

    create db update

--- db/Create.sql
+++ db/Create.sql
@@ -1,25 +1,56 @@
+DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-	userId bigint not null,
+	userId bigint not null AUTO_INCREMENT,
     name varchar(25) not null unique,
     password varchar(25) not null,
     points int not null default 1000,
     PRIMARY KEY (userId)
 );
 
-INSERT INTO users (userId, name, password, points) values
-(1,"Dimitri", "pass",1350),
-(2,"Lieven", "pass",1300),
-(3,"Kwinten", "pass",1250),
-(4,"Sara", "pass",1200),
-(5,"Peter", "pass",1150),
-(6,"Michael", "pass",1100),
-(7,"Christophe", "pass",1050),
-(8,"Wim", "pass",1000),
-(9,"Johan", "pass",950),
-(10,"Jessica", "pass",900),
-(11,"Jeroen", "pass",850),
-(12,"Sven", "pass",800),
-(13,"Joris", "pass",750),
-(14,"Tom", "pass",700)
+CREATE TABLE games(
+	gameId bigint not null AUTO_INCREMENT,
+    player1 bigint not null,
+    player2 bigint not null,
+    ranked tinyint not null,
+    winner bigint not null,
+    PRIMARY KEY(gameId),
+    FOREIGN KEY (player1)
+		REFERENCES users(userId)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (player2)
+		REFERENCES users(userId)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (winner)
+      REFERENCES users(userId)
+      ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO users (name, password, points) values
+("Dimitri", "pass",1350),
+("Lieven", "pass",1300),
+("Kwinten", "pass",1250),
+("Sara", "pass",1200),
+("Peter", "pass",1150),
+("Michael", "pass",1100),
+("Christophe", "pass",1050),
+("Wim", "pass",1000),
+("Johan", "pass",950),
+("Jessica", "pass",900),
+("Jeroen", "pass",850),
+("Sven", "pass",800),
+("Joris", "pass",750),
+("Tom", "pass",700);
+
+INSERT INTO games (player1, player2, ranked,winner) values
+(1,4,1,1),
+(4,7,1,2),
+(5,11,1,2),
+(3,2,0,1),
+(2,8,1,1),
+(2,4,1,2),
+(6,5,1,2),
+(8,9,0,1),
+(2,1,1,1),
+(1,9,1,2);
