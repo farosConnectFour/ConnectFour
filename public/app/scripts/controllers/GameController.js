bf606315cd450a8594f6ab76ff7060a5ff3661a6
juchtdi <dimitri.juchtmans@faros.be>
2015-09-17 17:11:40 +0200

    End watching init

--- public/app/scripts/controllers/GameController.js
+++ public/app/scripts/controllers/GameController.js
@@ -15,17 +15,8 @@
 
         $scope.$on("boardInfo", function(event, messageData){
             $scope.game = {board: messageData.game.game.board, currentPlayer: messageData.game.currentPlayer};
+            $scope.$apply();
         });
-
-        for (var row = 0; row < 6; row++) {
-            var cols = [];
-            for (var col = 0; col < 7; col++) {
-                cols.push(0);
-            }
-            board.push(cols);
-        }
-
-
     };
 
     angular.module("app").controller("GameController", ["$scope", "$location","$interval", "$routeParams", "GameService", gameController])
