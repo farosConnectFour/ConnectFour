f9c76dc5c78976d2825f6e16eede7074ed234c1e
Lieven Peeters <lieven.peeters@faros.be>
2015-10-23 10:52:53 +0200

    update workshop preparation

--- public/app/scripts/directives/mychatlist.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    "use strict";
-    angular.module("app").directive("myChatList", function(){
-       return{
-           restrict: "E",
-           templateUrl: "views/chat/chatlist.html"
-       }
-    });
-})();
